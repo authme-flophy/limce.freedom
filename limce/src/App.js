0a3bbbbffe8337079a10442ad16fbd6be2354be4
cynthia <cchelangat9892gmail.com>
2022-11-09 09:43:09 +0300

    styled the background image

--- limce/src/App.js
+++ limce/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Home from './components/Home';
 function App() {
   return (
     <div className="App">
       
 
 
-
+<Home/>
 
     </div>
   );
---
+++ limce/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+
+function Home() {
+  return (
+    <div className='home_container'>
+    {/* <img src='https://cdn.wallpapersafari.com/39/92/FoXNny.jpg'/> */}
+    <h1>Limce Freedom</h1>
+    
+    </div>
+  )
+}
+
+export default Home;
