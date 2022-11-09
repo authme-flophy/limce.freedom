9bddbc2fb4ff43891a4281d606524723ebe960b4
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-11-09 09:19:57 +0300

    configure routes for the project

--- limce/src/App.js
+++ limce/src/App.js
@@ -1,14 +1,20 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Album from './components/Album'
+import Footer from './components/Footer'
+import Home from './components/Home'
 function App() {
   return (
-    <div className="App">
-      
-
-
-
-
+    <div className='App'>
+      <Routes>
+        <Route path='/' index element={<Home />} />
+        <Route path='/albums' element={<Album />}>
+          <Route path=':songId' element={<Home />} />
+        </Route>
+      </Routes>
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
