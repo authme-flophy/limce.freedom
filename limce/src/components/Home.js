011155e9fd35b74161beb26d0c4d080c245b0d14
cynthia <cchelangat9892gmail.com>
2022-11-09 16:16:51 +0300

    EP-17

--- limce/src/components/Home.js
+++ limce/src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className='home_container'>
     {/* <img src='https://cdn.wallpapersafari.com/39/92/FoXNny.jpg'/> */}
-    <h1>Limce Freedom</h1>
+    <h1>Limce Freedom   .</h1>
     
     </div>
   )
