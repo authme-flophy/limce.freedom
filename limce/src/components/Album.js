33bf2e0505041dabf7dd2825f2b72d8ba0a17de0
cynthia <cchelangat9892gmail.com>
2022-11-10 11:12:07 +0300

    Added song lists to the album

--- limce/src/components/Album.js
+++ limce/src/components/Album.js
@@ -1,7 +1,41 @@
 import React from 'react'
+import './Album.css'
 
 const Album = () => {
-  return <div>albums section</div>
+  return (
+  <div className='album_container'>
+    <div className='album_content'>
+      <div className='album_image'>
+        <img className='image' src='https://res.cloudinary.com/dphlf7a8o/image/upload/v1668016652/her_loss_zlnjnz.jpg' alt='her_loss' />
+      </div>
+      <ul className='songs'>
+        <li>
+          <a href='#'>trending</a>
+          </li>
+        <li>
+          <a href='#'>popular</a>
+          </li>
+        <li>
+          <a href='#'>liked</a>
+          </li>
+        <li>
+          <a href='#'>playlist</a>
+        </li>
+
+      </ul>
+    </div>
+    <div className='iframe_container'>
+      <div className='iframe_reviews'>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/0jz0GAFNNIo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+       </div>
+    <div className='reviews'>
+      hfff
+
+    </div>
+  </div>
+  </div>
+  )
 }
 
 export default Album
